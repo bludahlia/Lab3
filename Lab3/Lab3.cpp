fecb397d9c233bc1ac4faa0ef9144be73a5b3d6d
Dahlia Mihalic <omihalic@purdue.edu>
2023-08-28 16:18:11 -0400

    peace

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -1,20 +1,62 @@
-// Lab3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+void printMenu(int& choice) {
+	cout << "Welcome to my program, please select an operation to perform:" << endl;
+	cout << "1. Addition" << endl;
+	cout << "2. Subtraction" << endl;
+	cout << "3. Multiplication" << endl;
+	cout << "4. Division" << endl;
+	cout << "\nYour Selection: ";
+	cin >> choice;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void getChoices(float& A, float& B) {
+	cout << "Please enter the first value:";
+	cin >> A;
+	cout << "Please enter the second value:";
+	cin >> B;
+}
+void addition(float A, float B) {
+	float C;
+	C = A + B;
+	cout << "Your results are: " << A << " + " << B << " = " << C << endl;
+}
+void subtraction(float A, float B) {
+	float C;
+	C = A - B;
+	cout << "Your results are: " << A << " - " << B << " = " << C << endl;
+}
+void multiplication(float A, float B) {
+	float C;
+	C = A * B;
+	cout << "Your results are: " << A << " * " << B << " = " << C << endl;
+}
+void division(float A, float B) {
+	float C;
+	C = A / B;
+	cout << "Your results are: " << A << " / " << B << " = " << C << endl;
+}
+int main() {
+	int choice;
+	float A;
+	float B;
+	printMenu(choice);
+	getChoices(A, B);
+	if (choice == 1) {
+		addition(A, B);
+	}
+	else
+		if (choice == 2) {
+			subtraction(A, B);
+		}
+		else
+			if (choice == 3) {
+				multiplication(A, B);
+			}
+			else
+				if (choice == 4) {
+					division(A, B);
+				}
+				else
+					cout << "Invalid choice" << endl;
+	return 0;
+}
